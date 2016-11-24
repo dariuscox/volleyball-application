2a8fcc020b3b9c2d84ec9899aabe72916648aaf3
DanielCox <ddc3372@rit.edu>
2016-11-24 17:42:44 -0500

    Player Class basic implementation

--- src/Player.java
+++ src/Player.java
@@ -3,4 +3,30 @@
  */
 public class Player {
     private String name;
+    private int number;
+    private String position;
+    Player(String name, int num){
+        this.name = name;
+        this.number = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String toString() {
+        return "Player{" + "name='" + name + '\'' + ", number=" + number + '}';
+    }
 }
--- src/TextMain.java
+++ src/TextMain.java
@@ -4,5 +4,7 @@
 public class TextMain {
     public static void main(String[] args) {
         System.out.println("this is where i print");
+        Player jim  = new Player("Jim", 9);
+        System.out.println(jim);
     }
 }

e5deba4dabd30a64858d5f8fa0439e4b2a0a7481
yxd3549 <yxd3549@cs.rit.edu>
2016-11-24 17:38:21 -0500

    StatsSheet created

---
+++ src/StatsSheet.java
@@ -0,0 +1,12 @@
+/**
+ * Created by DSYancarlos on 11/24/2016.
+ */
+public class StatsSheet {
+
+    private Integer Digs;
+    private Integer Assits;
+    private Integer Kills;
+    private Integer Aces;
+    private Integer Blocks;
+
+}

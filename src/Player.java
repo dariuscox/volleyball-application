84e80703e2dc4f53f1e4f221f7f608ce4a02c7c3
DanielCox <ddc3372@rit.edu>
2016-11-24 17:56:57 -0500

    added a record to team

--- src/Player.java
+++ src/Player.java
@@ -3,11 +3,13 @@
  */
 public class Player {
     private String name;
-    private int number;
+    private Integer number;
     private String position;
+    private StatsSheet stats;
     Player(String name, int num){
         this.name = name;
         this.number = num;
+        this.stats = new StatsSheet();
     }
 
     public String getName() {
--- src/Team.java
+++ src/Team.java
@@ -3,10 +3,19 @@
  */
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Team {
 
     private String teamName;
 
-    private ArrayList<Player> Roster;
+    private HashMap<String,Player> Roster;
+
+    private Integer wins;
+
+    private Integer loses;
+
+    public void printRecord(){
+        System.out.println("Win: " +wins+ " / Lose: " +loses);
+    }
 }
